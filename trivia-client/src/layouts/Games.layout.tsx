aea3df4263c5426bc7eb444b0926527d0b660f5e
mdever <disbrow.6@gmail.com>
2021-12-23 22:19:34 -0500

    Games layout

--- trivia-client/src/layouts/Games.layout.tsx
+++ trivia-client/src/layouts/Games.layout.tsx
@@ -1,10 +1,37 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { Outlet } from "react-router";
+import { AppContext } from "../context/app.context";
 
 export default function GamesLayout() {
+    const context = useContext(AppContext);
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        axios.post('/games', {
+            headers: {
+                'Authorization': `Bearer ${context.token}`
+            }
+        }).then(res => {
+            setGames(res.data);
+        }).catch(err => {
+            console.log('Could not fetch games');
+            console.log(err);
+            setGames([]);
+        });
+    }, []);
+
     return (
         <div className="grid grid-cols-layout">
-            <div className="games-list">
-                Games List
+            <div className="flex flex-column">
+                {
+                    games.map(g => {
+                        return (
+                            <div>{g.name}</div>
+                        );
+                    })
+                }
+                <div>New Game</div>
             </div>
             <div className="p-2">
                 Games Layout
